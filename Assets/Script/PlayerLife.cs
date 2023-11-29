25a8c58267e484e825b1af9a7f7d2851dc3a92d4
NicholasWiratama <innosyafei@gmail.com>
2023-11-29 21:19:19 +0700

    First Commit

---
+++ Assets/Script/PlayerLife.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PlayerLife : MonoBehaviour
+{
+    private Rigidbody2D io;
+    private Animator anim;
+    [SerializeField] private AudioSource DeathSoundEffect;
+
+    private void Start()
+    {
+        io = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Trap"))
+        {
+            Die();
+        }
+    }
+
+    private void Die()
+    {
+        DeathSoundEffect.Play();
+        io.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("Death");
+    }
+
+    private void RestartLevel()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
