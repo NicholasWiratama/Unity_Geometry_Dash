53731595edc404e1ee11e1b5516125dc5fe0d7a0
KrisErDim <140878957+KrisErDim@users.noreply.github.com>
2024-01-05 09:47:34 +0700

    aaasd

--- Assets/Script/Boss.cs
+++ Assets/Script/Boss.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class Boss : MonoBehaviour
 {
     public GameObject[] prefabs;
-    float spawnRate = 2f; // Adjust this based on your desired spawn frequency
+    float spawnRate = 2f; 
     float nextSpawnTime = 0f;
 
     [SerializeField] private Transform player;
@@ -21,10 +21,9 @@ public class Boss : MonoBehaviour
         Debug.Log(transform.position);
     }
 
-    // Spawn Prefabs
     void SpawnPrefabs() {
         if (Time.time >= nextSpawnTime) {
-            nextSpawnTime = Time.time + 2f / spawnRate; // Update the spawn time
+            nextSpawnTime = Time.time + 2f / spawnRate;
 
             Vector3 spawnPosition = ChooseSpawnPosition();
             InstantiateRandomPrefab(spawnPosition);
@@ -32,13 +31,11 @@ public class Boss : MonoBehaviour
     }
 
     Vector3 ChooseSpawnPosition() {
-        // Choose a random position among the three points (top, middle, bottom)
-        float randomY = Random.Range(-6f, 1f); // Adjust the Y values based on your scene
+        float randomY = Random.Range(-6f, 1f); 
         return new Vector3(transform.position.x, randomY, 0);
     }
 
     void InstantiateRandomPrefab(Vector3 spawnPosition) {
-        // Replace "YourPrefab" with the actual prefab you want to spawn
         int randomIndex = Random.Range(0, prefabs.Length);
         Instantiate(prefabs[randomIndex], spawnPosition, Quaternion.identity);
     }
